9a790c9009b71c39719ca2b97e25a564cba20234
Leen151 <leen.m@orange.fr>
2024-10-20 21:08:01 +0200

    mise en place du header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Logo from "../../assets/Logo.svg"
+import "./header.scss";
 
 export const Header = () => {
   return (
-    <header>Header</header>
+    <header className='header'>
+      <img src={Logo} alt="logo SportSee" className="logo-header"></img>
+      <nav className='navbar-primary'>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink>Profil</NavLink>
+        <NavLink>Réglage</NavLink>
+        <NavLink>Communauté</NavLink>
+      </nav>
+
+    </header>
   )
 }
