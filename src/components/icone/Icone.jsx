4bbee02bc48fc097f970d0a84a16901fa09713ee
Leen151 <leen.m@orange.fr>
2024-10-20 21:47:26 +0200

    mise en place du menu secondaire + composant icone réutilisable

---
+++ src/components/icone/Icone.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./icone.scss"
+
+export const Icone = ({icone, bg}) => {
+  return (
+    <div className={`icone ${bg}`}>
+      <img src={icone} alt=""/>
+    </div>
+  )
+}
--- src/components/navbarSecondary/NavbarSecondary.jsx
+++ src/components/navbarSecondary/NavbarSecondary.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import "./navbarSecondary.scss"
+import { Icone } from '../icone/Icone'
+
+import Bike from "../../assets/iBike.svg"
+import Yoga from "../../assets/iYoga.svg"
+import Swim from "../../assets/iSwimming.svg"
+import Bodybuilding from "../../assets/iBodybuilding.svg"
+
 
 export const NavbarSecondary = () => {
   return (
-    <div>NavbarSecondary</div>
+    <div className='sidebar'>
+      <nav className='navbar-secondary'>
+        <Icone icone={Yoga} bg="bg-common"/>
+        <Icone icone={Swim} bg="bg-common"/>
+        <Icone icone={Bike} bg="bg-common"/>
+        <Icone icone={Bodybuilding} bg="bg-common"/>
+      </nav>
+
+      <div className='copyright'>Copyright, SportSee 2020</div>
+    </div>  
   )
 }
