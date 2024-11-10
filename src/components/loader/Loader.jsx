02a102ba2af8e39bfb695cd82c533b72dd1d66fc
Leen151 <leen.m@orange.fr>
2024-11-10 22:20:06 +0100

    création d'un composant affichant un message pendant les chargement des données

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./loader.scss";
+
+export const Loader = () => {
+  return (
+    <div className="loader">
+      Chargement des données...
+    </div>
+  );
+};
